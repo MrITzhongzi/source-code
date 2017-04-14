1c336c98059b97d3e1df01c42ce085c4373687a7
lhw <lhw@qq.com>
2017-04-14 11:30:57 +0800

    vue学习第一天demo

---
+++ VUE/01天learningVue/component组件/sliderShow.js
@@ -0,0 +1,4 @@
+
+var SliderShow = {
+    template : '<div style="background: #ff3c55">我是顶部的轮播图</div>'
+};
---
+++ VUE/01天learningVue/component组件/tab.js
@@ -0,0 +1,4 @@
+
+var Tab = {
+    template : '<ul style="position: fixed;bottom: 0;background: #288077;width: 100%"><li>我是tab</li></ul>'
+};
