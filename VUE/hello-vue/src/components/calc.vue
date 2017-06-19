1c68b0c33fae0a65f5c35341ccc1b8ffc82d39d4
lhw <739619784@qq.com>
2017-06-19 15:49:03 +0800

    vue   element UI 测试

---
+++ VUE/hello-vue/src/components/calc.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-input-number v-model="num1" @change="handleChange" :min="1" :max="10"></el-input-number>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                num1: 1
+            };
+        },
+        methods: {
+            handleChange(value) {
+                console.log(value);
+            }
+        }
+    };
+</script>
---
+++ VUE/hello-vue/src/components/ele-color.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>ele 颜色</h1>
+        <h2>ele 字体</h2>
+        <div class="tubiao">
+            <i class="el-icon-edit"></i>
+            <i class="el-icon-share"></i>
+            <i class="el-icon-delete"></i>
+            <i class="el-icon-arrow-down"></i>
+            <el-button type="primary" icon="search">搜索</el-button>
+        </div>
+        <div>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="text">文字按钮</el-button>
+        </div>
+        <div>
+            <el-button type="primary" icon="edit"></el-button>
+            <el-button type="primary" icon="share"></el-button>
+            <el-button type="primary" icon="delete"></el-button>
+            <el-button type="primary" icon="search">搜索</el-button>
+            <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    div {
+        h1 {
+            color : #58B7FF;
+        }
+        h2 {
+            font-family: Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+        }
+    }
+</style>
+
+<script>
+
+</script>
