1f1655f24ad7b1529a175ee2388755738ffd0b20
lhw <739619784@qq.com>
2017-07-03 09:35:07 +0800

    node demo

---
+++ nodejs/testDemo/modalSystem/01/main.js
@@ -0,0 +1,4 @@
+
+const hello = require('./hello');
+
+hello.world();
---
+++ nodejs/testDemo/modalSystem/02/main.js
@@ -0,0 +1,9 @@
+const Hello = require('./hello');
+
+hello = new Hello();
+
+hello.setName('lhw');
+
+hello.sayHello();
+
+console.log('程序执行完毕');
