7325bb87746cf3a2f3ebea0f24e00c6726c257e9
lhw <739619784@qq.com>
2017-08-21 09:56:57 +0800

    智惠东静态页

--- 静态页面/智惠东静态页面/zhihd-static/script/utils/pub_function.js
+++ 静态页面/智惠东静态页面/zhihd-static/script/utils/pub_function.js
@@ -1,7 +1,27 @@
 var utils = {}
 
-utils.setCss = function (element,styleObj) {
-    for(var key in styleObj){
+utils.setCss = function (element, styleObj) {
+    for (var key in styleObj) {
         element.style[key] = styleObj[key]
     }
+}
+
+// 获取浏览器窗口的可视区域的宽度
+utils.getViewPortWidth = function () {
+    return document.documentElement.clientWidth || document.body.clientWidth;
+}
+
+// 获取浏览器窗口的可视区域的高度
+utils.getViewPortHeight = function () {
+    return document.documentElement.clientHeight || document.body.clientHeight;
+}
+
+// 获取浏览器窗口水平滚动条的位置
+utils.getScrollLeft = function () {
+    return document.documentElement.scrollLeft || document.body.scrollLeft;
+}
+
+// 获取浏览器窗口垂直滚动条的位置
+utils.getScrollTop = function () {
+    return document.documentElement.scrollTop || document.body.scrollTop;
 }
